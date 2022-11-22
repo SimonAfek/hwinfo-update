b5ba66f216280b67bef62a4ba4785b88f524dc91
SimonAfek <simonaj92@gmail.com>
2022-11-22 11:32:20 +0100

    Disable progress bar + update user agent

--- hwinfo-update.ps1
+++ hwinfo-update.ps1
@@ -1,4 +1,6 @@
-$userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
+$ProgressPreference = 'SilentlyContinue'
+
+$userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 
 $page = Invoke-WebRequest -Uri 'https://www.fosshub.com/HWiNFO.html' -Method Get -UserAgent $userAgent
 $settingsString = ''
